b8e979cba8dbc7a4e769df2b2992f1ecfa133bc3
Mark Heijnekamp <mark@heijnekamp.nl>
2021-03-21 00:30:59 +0100

    Updated: - Git ignore Removed: - target folder

--- target/rls/debug/deps/hangman-eb815ab69d87b19e.d
+++
@@ -1,7 +0,0 @@
-/Users/markheijnekamp/Projects/rust/hangman/target/rls/debug/deps/hangman-eb815ab69d87b19e.rmeta: src/main.rs src/game.rs src/menu.rs
-
-/Users/markheijnekamp/Projects/rust/hangman/target/rls/debug/deps/hangman-eb815ab69d87b19e.d: src/main.rs src/game.rs src/menu.rs
-
-src/main.rs:
-src/game.rs:
-src/menu.rs:
